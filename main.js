24494e2ff279945033d5aa378f6bee01e57e81af
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2025-05-28 14:14:46 -0300

    654321

--- main.js
+++ main.js
@@ -11,4 +11,24 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date ("2026-01-01T00:00:00");
+const tempoObjetivo1 = new Date ("2026-06-01T00:00:00");
+const tempoObjetivo2 = new Date ("2025-09-27T19:00:00");
+const tempoObjetivo3 = new Date ("2027-12-12T00:00:00");
+const tempoObjetivo4 = new Date ("2025-09-29T06:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+function atualizaCronometro (){
+    for (let i = 0; i < contadores.length; i++){
+        document.getElementById ("dias"+i).textContent = calculaTempo(tempos [i])[0];
+        document.getElementById ("horas"+i).textContent = calculaTempo(tempos [i])[1];
+        document.getElementById ("min"+i).textContent = calculaTempo(tempos [i])[2];
+        document.getElementById ("seg"+i).textContent = calculaTempo(tempos [i])[3];
+    }
+    for (let i = 0; i < contadores.length; i++){
+        //contadores[i].textContent = calculaTempo (tempos [i]);
+    }
+}
+function comecaCronometro (){
+    atualizaCronometro ();
+    setInterval (atualizaCronometro, 1000);
+}
+comecaCronometro();
